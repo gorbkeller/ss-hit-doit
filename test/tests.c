4de74a7389210194765b3cff1f6204d22cdfd3e5
Gordon Keller <gordon.h.keller@gmail.com>
2021-12-13 19:20:55 -0800

    Worked more on the implicit tests

--- test/tests.c
+++ test/tests.c
@@ -57,6 +57,15 @@
 
 #define MAX_PATH_BUF 256
 
+
+typedef enum PathType
+{
+    FILEPATH = 0,
+    DIRPATH = 1,
+    UNKPATH = 2,
+    ERRPATH = -1
+} PathType;
+
 int assert_success( ret_val )
 {
     if ( ret_val == 0 )
@@ -85,6 +94,35 @@ void print_test_result( int test_result, const char *func_name, const char *test
         char result[] = "\033[1;31mFAILED\033[0m";
         printf( "%s (%s): %s\n", func_name, test_name, result );
     }
+}
+
+PathType identify_path_type(char *path)
+{
+    // this function determines if the path provided is a file or a directory
+    struct stat s;
+    if( stat(path,&s) == 0 )
+    {
+        if( s.st_mode & S_IFDIR )
+        {
+            //it's a directory
+            return DIRPATH;
+        }
+        else if( s.st_mode & S_IFREG )
+        {
+            //it's a file
+            return FILEPATH;
+        }
+        else
+        {
+            //something else
+            return UNKPATH;
+        }
+    }
+    else
+    {
+        fprintf(stderr, "ERROR:  %s is not a valid path name.\n", name);
+        
+    } 
 }
 
 int make_dummy_file( const char *fName )
@@ -225,17 +263,88 @@ int test_ss_symlink_single_f_explicit()
     return 0;
 }
 
-/*
-int test_ss_symlink_single_f_implicit()
-{
 
-}*/
+int test_ss_symlink_single_f_implicit(void)
+{
+    // get the current working directory -- assume that the absolute path
+    // will be provided by default in the actual code
+    char path[MAX_PATH_BUF];
+    getcwd( path, MAX_PATH_BUF );
+    const char test_fName[] = "temp/test.txt";
+    const char test_aDir[] = "temp/links/";
+    char full_fName[256];
+    char full_aDir[256];
+
+    sprintf( full_fName, "%s/%s", path, test_fName );
+    sprintf( full_aDir, "%s/%s", path, test_aDir );
+
+    // create test file
+    make_dummy_file( full_fName );
+
+    // determine if the provided original path is a directory or a file
+    PathType fName_path_type = identify_path_type( full_fName );
+
+    // currently, we can't process directories as the origin. need to implement later.
+    if ( fName_path_type == DIRPATH )
+    {
+        fprintf( stderr, "ERROR: cannot use path type of directory for linking. Argument passed: %s", full_fName );
+        remove( full_fName );
+        return -1;
+    } else if ( fName_path_type == ERRPATH )
+    {
+        fprintf( stderr, "ERROR: The path passed throws an error. Argument passed: %s", full_fName );
+        remove( full_fName );
+        return -1;
+    } else if ( fName_path_type == UNKPATH )
+    {
+        fprintf( stderr, "ERROR: The path passed is of an unknown type -- only files are allowed. Argument passed: %s", full_fName );
+        remove( full_fName );
+        return -1;
+    }
+
+
+    // determine if the provided original path is a directory or a file
+    aName_path_type = identify_path_type(full_aName);
+
+    // if a directory is passed, append the name of the original file to the end 
+    if ( fName_path_type == DIRPATH )
+    {
+        int ret_val = alias_dir_to_file( full_aName, full_fName );
+        remove( full_fName );
+        return -1;
+    } else if ( fName_path_type == ERRPATH )
+    {
+        fprintf( stderr, "ERROR: The path passed throws an error. Argument passed: %s", full_fName );
+        remove( full_fName );
+        return -1;
+    } else if ( fName_path_type == UNKPATH )
+    {
+        fprintf( stderr, "ERROR: The path passed is of an unknown type -- only files are allowed. Argument passed: %s", full_fName );
+        remove( full_fName );
+        return -1;
+    }
+
+    // link test file to alias
+    int ret_val = symlink( full_fName, full_aDir );
+    if ( ret_val != 0 )
+    {
+        char err_msg[256];
+        sprintf(err_msg, "ERROR: could not create symlink %s -> %s\n", full_fName, full_aDir );
+        perror( err_msg );
+    }
+        
+    int test_result = assert_success( ret_val );
+    print_test_result( test_result , __func__ , "a1" );
+
+    return 0;
+
+}
 
 int main( void )
 {
     // run tests
-    test_ss_symlink_single_f_explicit();
-    //test_ss_symlink_single_f_implicit();
+    //test_ss_symlink_single_f_explicit();
+    test_ss_symlink_single_f_implicit();
 
     return 0;
 
